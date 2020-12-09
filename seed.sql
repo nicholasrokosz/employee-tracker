4422ad42a79ab0b52e416c6dd5353a1287696fcf
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-12-08 20:00:16 -0700

    add seed.sql

---
+++ seed.sql
@@ -0,0 +1,28 @@
+USE staff_db;
+INSERT INTO
+  department(name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Finace'),
+  ('Legal');
+INSERT INTO
+  role(title, salary, department_id)
+VALUES('Sales Lead', 100000, 1),
+  ('Salesperson', 80000, 1),
+  ('Lead Engineer', 150000, 2),
+  ('Software Engineer', 120000, 2),
+  ('Accountant', 125000, 3),
+  ('Legal Team Lead', 250000, 4),
+  ('Lawyer', 190000, 4);
+INSERT INTO
+  employee(first_name, last_name, role_id, manager_id)
+VALUES
+  ('John', 'Doe', 1, 3),
+  ('Mike', 'Chan', 2, 1),
+  ('Ashley', 'Rodriguez', 3, null),
+  ('Kevin', 'Tupik', 4, 3),
+  ('Malia', 'Brown', 5, null),
+  ('Sarah', 'Lourd', 6, null),
+  ('Tom', 'Allen', 7, 6),
+  ('Christian', 'Eckenrode', 3, 2);
